6b1fe7a51c32d7ed01378f6ad578b9dcfa76ceed
enrico <enrico.dsf@gmail.com>
2024-01-28 17:43:01 +0100

    d3 solution

--- day03/solution.py
+++ day03/solution.py
@@ -5,12 +5,15 @@ def is_possible(a, b, c):
     return (a + b > c) and (b + c > a) and (a + c > b)
 
 
+def all_possible(triangles):
+    return sum(is_possible(*triangles[i : i + 3]) for i in range(0, len(triangles), 3))
+
+
 with open("data") as f:
     triangles = [int(x) for x in re.findall(r"\d+", f.read())]
 
 # ==== PART 1 ====
-print(sum(is_possible(*triangles[i:i+3]) for i in range(0, len(triangles), 3)))
+print(all_possible(triangles))
 
 # ==== PART 2 ====
-triangles = triangles[::3] + triangles[1::3] + triangles[2::3]
-print(sum(is_possible(*triangles[i:i+3]) for i in range(0, len(triangles), 3)))
+print(all_possible(triangles[::3] + triangles[1::3] + triangles[2::3]))
