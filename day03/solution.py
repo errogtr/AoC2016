61d6d4a9ec6bc33013062f928565e8ee0fcd2e5f
enrico <enrico.dsf@gmail.com>
2024-01-29 00:56:55 +0100

    d3 refactor

--- day03/solution.py
+++ day03/solution.py
@@ -1,19 +1,17 @@
-import re
-
-
 def is_possible(a, b, c):
-    return (a + b > c) and (b + c > a) and (a + c > b)
-
-
-def all_possible(triangles):
-    return sum(is_possible(*triangles[i : i + 3]) for i in range(0, len(triangles), 3))
+    return a + b > c
 
 
 with open("data") as f:
-    triangles = [int(x) for x in re.findall(r"\d+", f.read())]
+    triangles = [[int(x) for x in line.split()] for line in f.read().splitlines()]
 
 # ==== PART 1 ====
-print(all_possible(triangles))
+print(sum(is_possible(*sorted(t)) for t in triangles))
 
 # ==== PART 2 ====
-print(all_possible(triangles[::3] + triangles[1::3] + triangles[2::3]))
+print(
+    sum(
+        sum(is_possible(*sorted(t)) for t in zip(*triangles[i:i + 3]))
+        for i in range(0, len(triangles), 3)
+    )
+)
