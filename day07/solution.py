b03aa3c46ffd496d2cd6d3abe42b73f8fdf13780
enrico <enrico.dsf@gmail.com>
2024-01-31 01:18:04 +0100

    d7 aligned part 1 and 2

--- day07/solution.py
+++ day07/solution.py
@@ -6,13 +6,15 @@ with open("data") as f:
 
 
 # ==== PART 1 ====
-supernet = re.compile(r"(?:^|])\w*(\w)((?!\1)\w)\2\1\w*(?:\[|$)")  # search for 'abba' outside '[]'
-hypernet = re.compile(r"\[\w*(\w)((?!\1)\w)\2\1\w*]")  # search for 'abba' inside '[]'
-print(sum(supernet.search(ip) is not None and not hypernet.search(ip) for ip in ips))
+abba = [
+    re.compile(r"(?:^|])\w*(\w)((?!\1)\w)\2\1\w*(?:\[|$)"),  # ]...abba...[
+    re.compile(r"^(?!.*\[\w*(\w)((?!\1)\w)\2\1\w*])"),  # NOT [...abba...]s
+]
+print(sum(all(r.search(ip) for r in abba) for ip in ips))
 
 # ==== PART 2 ====
 aba = [
     re.compile(r"(?:^|])\w*(\w)((?!\1)\w)\1.*\[\w*\2\1\2"),  # ...aba...[...bab...]
-    re.compile(r"\[\w*(\w)((?!\1)\w)\1\w*].*\2\1\2\w*(?:\[|$)"),   # ...[...aba...]...bab...
+    re.compile(r"\[\w*(\w)((?!\1)\w)\1\w*].*\2\1\2\w*(?:\[|$)"),  # ...[...bab...]...aba...
 ]
 print(sum(any(r.search(ip) for r in aba) for ip in ips))
