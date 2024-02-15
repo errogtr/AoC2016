1aec5039c9ad8ce0c07e118155bd241d323310be
enrico <enrico.dsf@gmail.com>
2024-02-15 01:48:40 +0100

    d19 refactor

--- day19/solution.py
+++ day19/solution.py
@@ -1,23 +1,20 @@
+from math import log, floor
+
 with open("data") as f:
     N = int(f.read())
 
 # ==== PART 1 ====
-# write any number N as 2^n + k, where n is the largest number s.t. 2^n < N, and 0 <= k < 2^n.
+# write any number N as 2^n + k, where n is the largest integer s.t. 2^n < N, and 0 <= k < 2^n.
 # Then: f(2^n + k) = 2k + 1
 
-n = 1
-while 2**n < N:
-    n += 1
-print(2 * (N - 2 ** (n - 1)) + 1)
+print(2 * (N - 2 ** floor(log(N, 2))) + 1)
 
 # ==== PART 2 ====
-# write any number N as 3^n + k, where n is the largest number s.t. 3^n < N, and 1 <= k < 2 * 3^n.
+# write any number N as 3^n + k, where n is the largest integer s.t. 3^n < N, and 1 <= k < 2 * 3^n.
 # Then:
 #               |- k,          if 1 <= k < 3^n
-# f(2^n + k) = -|
+# f(3^n + k) = -|
 #               |- 2k - 3^n,   if 3^n <= k < 2*3^n
 
-n = 1
-while 3**n < N:
-    n += 1
-print(k if 1 <= (k := (N - 3 ** (n - 1))) < 3 ** (n - 1) else 2 * k - 3 ** (n - 1))
+l = 3 ** floor(log(N, 3))
+print(k if 1 <= (k := (N - l)) < l else 2 * k - l)
